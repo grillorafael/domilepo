473e15af75898ad920e63e5c905c3c3a9fe8598e
Rafael Grillo <grillorafael@gmail.com>
2014-05-10 10:41:37 -0300

    refatoração do tcp

--- tcp_server.py
+++ tcp_server.py
@@ -1,26 +1,31 @@
-# REFERENCE: https://docs.python.org/2.7/library/socketserver.html
-import SocketServer
+#!/usr/bin/python
 from socket import *
-from terminal_colors import *
-
-class TcpHandler(SocketServer.BaseRequestHandler):
-  def handle(self):
-    # self.request is the TCP socket connected to the client
-    self.data = self.request.recv(1024).strip()
-    print "{} wrote:".format(self.client_address[0])
-    print self.data
-    # just send back the same data, but upper-cased
-    self.request.sendall(self.data.upper())
 
 class TcpServer:
   def __init__(self, game):
     self.game = game
 
-    HOST, PORT = "127.0.0.1", 3000
-    # Create the server, binding to localhost on port 9999
-    server = SocketServer.TCPServer((HOST, PORT), TcpHandler)
+    s = socket(AF_INET, SOCK_STREAM)    # create a TCP socket
+    s.bind(('127.0.0.1', 3000))            # bind it to the server port
+    s.listen(5)                         # allow 5 simultaneous # pending connections
+    print "Listening to http://127.0.0.1:3000\n"
 
-    # Activate the server; this will keep running until you
-    # interrupt the program with Ctrl-C
-    print Colors.red("Server started at: http://{HOST}:{PORT}".format(HOST=HOST, PORT=PORT))
-    server.serve_forever()
+    while 1:
+        # wait for next client to connect
+        connection, address = s.accept() # connection is a new socket
+        self.game.connectPlayer(connection)
+        self.broadcastMessage("New player connected\n")
+        self.broadcastMessage("Waiting for more players to connect...\n")
+
+        # while 1:
+        #     data = connection.recv(1024) # receive up to 1K bytes
+        #     if data:
+        #         connection.send('echo -> ' + data)
+        #     else:
+        #         break
+        # connection.close()              # close socket
+
+  def broadcastMessage(self, message):
+    for p in self.game.connectedPlayers():
+      print "{}\n".format(message)
+      p.connection.send(message)
